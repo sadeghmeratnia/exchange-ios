4ffb9098b7cf3a5d78f6d0c8f512a73e28d32e5d
Sadegh <sadegh_meratnia@outlook.com>
2026-05-30 13:17:49 +0100

    Set up base iOS SwiftUI project

---
+++ Exchange/ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApp.swift
+//  Exchange
+//
+//  Created by Sadegh on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExchangeTests/ExchangeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeTests.swift
+//  ExchangeTests
+//
+//  Created by Sadegh on 30/05/2026.
+//
+
+import Testing
+@testable import Exchange
+
+struct ExchangeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
