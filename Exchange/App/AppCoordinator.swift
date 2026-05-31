190f0eebcc2ec63d21fff50828d0cb7e98b33d2a
Sadegh <sadegh_meratnia@outlook.com>
2026-05-31 11:35:09 +0100

    Scaffold app coordinator and wire app root composition

---
+++ Exchange/App/AppCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  AppCoordinator.swift
+//  Exchange
+//
+//  Created by Sadegh on 31/05/2026.
+//
+
+import SwiftUI
+
+// MARK: - AppCoordinator
+
+struct AppCoordinator: View {
+    private let appContainer: AppContainer
+
+    init(appContainer: AppContainer) {
+        self.appContainer = appContainer
+    }
+
+    var body: some View {
+        ContentView()
+    }
+}
--- Exchange/ExchangeApp.swift
+++ Exchange/ExchangeApp.swift
@@ -5,13 +5,33 @@
 //  Created by Sadegh on 30/05/2026.
 //
 
+import Foundation
 import SwiftUI
 
 @main
 struct ExchangeApp: App {
+    private let appContainer: AppContainer
+
+    init() {
+        let logger = OSNetworkLogger()
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+
+        let networkClient = URLSessionNetworkClient(
+            session: URLSession.shared,
+            decoder: decoder,
+            retryPolicy: DefaultRetryPolicy(),
+            logger: logger)
+
+        self.appContainer = AppContainer(
+            networkClient: networkClient,
+            logger: logger)
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppCoordinator(appContainer: appContainer)
         }
     }
 }
